d514c8055a84aff69eaf31a1671622beb7fa3c21
Irene <irene.iaccio@gmail.com>
2019-11-05 20:17:40 +0100

    Fix crypto

--- src/user/service.ts
+++ src/user/service.ts
@@ -7,16 +7,16 @@ import crypto = require('crypto');
 export class userService {
 
     public addUser(req: Request, res: Response) {
-        const newUser = new User(req.body);
         let salt = crypto.randomBytes(16).toString('base64');
         let hash = crypto.createHmac('sha512',salt).update(req.body.password).digest("base64");
-        req.body.password = salt + "$" + hash;
-        req.body.permissionLevel = 1;
+        req.body.password = salt + '$' + hash;
+        const newUser = new User(req.body);
+        // if user doesn't exist
         newUser.save((error: Error, User: MongooseDocument) => {
             if (error) {
                 res.send(error);
             }
-            res.send(User.id);
+            res.status(200).send({id: User.id});
         });
     }
 
