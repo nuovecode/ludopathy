e21e8a61d086ad9c975b8f6d63493f4ce10d573c
Irene <irene.iaccio@gmail.com>
2019-11-06 19:20:06 +0100

    Unique email validation

--- src/user/model.ts
+++ src/user/model.ts
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     //email: { type: String, required: [true, 'Required field']},
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     boards: { type: Array }
 });
--- src/user/service.ts
+++ src/user/service.ts
@@ -6,11 +6,13 @@ import crypto = require('crypto');
 
 export class userService {
 
+
     public addUser(req: Request, res: Response) {
         let salt = crypto.randomBytes(16).toString('base64');
         let hash = crypto.createHmac('sha512',salt).update(req.body.password).digest("base64");
         req.body.password = salt + '$' + hash;
         const newUser = new User(req.body);
+
         newUser.save((error: Error, User: MongooseDocument) => {
             if (error) {
                 res.send(error);
